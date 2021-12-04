84d7876ece6c4a82bcc735cc7714ba819c8d56f1
Edy koffi <koffiedy@gmail.com>
2021-12-04 06:58:01 +0000

    try catch in int funciotn

--- bin/init/index.js
+++ bin/init/index.js
@@ -5,12 +5,17 @@ const { join } = require('path')
 function init() {
   return new Promise(async (resolve, reject) => {
 
-    createFolder(join(process.cwd(), '.cdep'))
-    createFolder(join(process.cwd(), '.cdep', 'data'))
-    createFolder(join(process.cwd(), '.cdep', 'keys'))
+    try {
+      createFolder(join(process.cwd(), '.cdep'))
+      createFolder(join(process.cwd(), '.cdep', 'data'))
+      createFolder(join(process.cwd(), '.cdep', 'keys'))
 
-    file.writePass()
+      file.writePass()
+      resolve("File created")
 
+    } catch (error) {
+      reject(error.message)
+    }
   })
 }
 
